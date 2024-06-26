d7ac79fbe112a5109a4248f7f63c64af6aa6298c
Erenkx <erenkx@icloud.com>
2024-06-26 16:04:50 +0800

    Add an example from the book to illustrate how concatenation works with integers and strings

---
+++ ch02/StrConcat.java
@@ -0,0 +1,10 @@
+public class StrConcat {
+
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + "Hello");
+        // the output is 3Hello
+
+        System.out.println("Hello" + 1 + 2);
+        // the output is Hello12
+    }
+}

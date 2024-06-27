5477144959264034feb0e7f4ad4e4fda1cd1bfb0
Erenkx <erenkx@icloud.com>
2024-06-27 20:37:55 +0800

    Add solution to exercise 3.2

---
+++ ch03/Temperature.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Converts a temperature from Celsius to Fahrenheit.
+ */
+
+public class Temperature {
+
+    public static void main(String[] args) {
+        double celsius, fahrenheit;
+        Scanner in = new Scanner(System.in);
+
+        // prompt the user and get the value
+        System.out.print("Enter a temperature in Celsius: ");
+        celsius = in.nextDouble();
+
+        // convert and output the result
+        fahrenheit = celsius * 9 / 5 + 32;
+        System.out.printf("%.1f C = %.1f F\n", celsius, fahrenheit);
+    }
+}

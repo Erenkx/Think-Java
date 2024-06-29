d4fea4bbf80529779b59193e4ed7c4cc4caf6432
Erenkx <erenkx@icloud.com>
2024-06-29 16:03:08 +0800

    Add solution to exercise 4.1

---
+++ ch04/Date.java
@@ -0,0 +1,29 @@
+public class Date {
+
+    public static void printAmerican(String day, int date,
+                                     String month, int year) {
+        System.out.println(
+            "American format: " + day + ", " +
+            month + " " + date + ", " + year
+        );
+    }
+
+    public static void printEuropean(String day, int date,
+                                     String month, int year) {
+        System.out.println(
+            "European format: " + day + " " +
+            date + " " + month + " " + year
+        );
+    }
+
+    public static void main(String[] args) {
+        String day = "Saturday";
+        int date = 29;
+        String month = "June";
+        int year = 2024;
+
+        printAmerican(day, date, month, year);
+        printEuropean(day, date, month, year);
+    }
+
+}

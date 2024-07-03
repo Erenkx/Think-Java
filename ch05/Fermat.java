2ac04c1af03c8a3726d17215ddd924316d74dc92
Erenkx <erenkx@icloud.com>
2024-07-03 20:27:37 +0800

    Fix the definition of the Fermat's Last Theorem and add error handlers for negative numbers

--- ch05/Fermat.java
+++ ch05/Fermat.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Checks to see if Fermat's Last Theorem holds.
  *
  * Fermat's Last Theorem:
- *     There are no integers a, b, c, and n such that
+ *     There are no positive integers a, b, c, and n such that
  *     a^n + b^n = c^n, except when n <= 2.
  */
 public class Fermat {
@@ -18,8 +18,13 @@ public class Fermat {
         System.out.print("a: ");
         if (in.hasNextInt()){
             a = in.nextInt();
+            if (a <= 0) {
+                System.err.println("Error: a must be a positive integer.");
+                return;
+            }
         } else {
             word = in.next();
+            System.err.print("Error: ");
             System.err.println(word + " is not an integer.");
             return;
         }
@@ -27,8 +32,13 @@ public class Fermat {
         System.out.print("b: ");
         if (in.hasNextInt()) {
             b = in.nextInt();
+            if (b <= 0) {
+                System.err.println("Error: b must be a positive integer.");
+                return;
+            }
         } else {
             word = in.next();
+            System.err.print("Error: ");
             System.err.println(word + " is not an integer.");
             return;
         }
@@ -36,8 +46,13 @@ public class Fermat {
         System.out.print("c: ");
         if (in.hasNextInt()) {
             c = in.nextInt();
+            if (c <= 0) {
+                System.err.println("Error: c must be a positive integer.");
+                return;
+            }
         } else {
             word = in.next();
+            System.err.print("Error: ");
             System.err.println(word + " is not an integer.");
             return;
         }
@@ -45,8 +60,13 @@ public class Fermat {
         System.out.print("n: ");
         if (in.hasNextInt()) {
             n = in.nextInt();
+            if (n <= 0) {
+                System.err.println("Error: n must be a positive integer.");
+                return;
+            }
         } else {
             word = in.next();
+            System.err.print("Error: ");
             System.err.println(word + " is not an integer.");
             return;
         }

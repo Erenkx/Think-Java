45d475fdd94cb50d7e56fa97d37fdab24de0e7fd
Erenkx <erenkx@icloud.com>
2024-07-12 11:01:27 +0800

    Fix a typo

--- ch10/Center.java
+++ ch10/Center.java
@@ -7,7 +7,7 @@ public class Center {
         Rectangle box1 = new Rectangle(2, 4, 7, 9);
         Point p1 = findCenter(box1);    // (5, 8)
         printPoint(p1);
-g
+
         box1.grow(1, 1);    // (1, 3, 9, 11)
         Point p2 = findCenter(box1);    // (5, 8)
         printPoint(p2);

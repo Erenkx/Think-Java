18ba0b5a917a8e32bc453c774fb7f1b95ae507bc
Erenkx <erenkx@icloud.com>
2024-11-17 09:15:28 +1100

    Fix a typo

--- ch03/GuessStarter.java
+++ ch03/GuessStarter.java
@@ -15,7 +15,7 @@ public class GuessStarter {
         int number = random.nextInt(100) + 1;
 
         // prompt the user and get the value
-        System.out.println("I'm thiking of a number between 1 and 100 " +
+        System.out.println("I'm thinking of a number between 1 and 100 " +
             "(including both). Can you guess what it is?");
         System.out.print("Type a number: ");
         guess = in.nextInt();
